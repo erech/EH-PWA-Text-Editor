788a3659783fc2818b70315e12bdd060d218af48
erech <sofiahanapin@gmail.com>
2023-03-05 12:19:54 -0800

    add plugins + css loaders + babel

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -18,13 +18,60 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      //generates HTML file with script tags
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        inject: 'body'
+      }),
+      //generates web app manifest file w/ metadata and assets
+      new WebpackPwaManifest({
+        name: 'Text Editor',
+        short_name: 'JATE',
+        description: 'A text editor for your text use',
+        background_color: '#ffffff',
+        theme_color: '#2196f3',
+        icons: [
+          {
+            src: path.resolve('src/assets/icon.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons')
+          }
+        ]
+      }),
+      //generates service worker file + inject to build
+      new InjectManifest({
+        swSrc: './src/sw.js',
+        swDest: 'sw.js',
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024 // 5MB
+      })
     ],
 
     module: {
       rules: [
-        
-      ],
-    },
+        {
+          //css + style loader
+          test: /\.css$/i,
+          use: [
+          'style-loader', 
+          'css-loader'
+        ]
+        },
+        {
+          //babel loader
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+              plugins: [
+                '@babel/plugin-proposal-object-rest-spread', 
+                '@babel/transform-runtime'
+              ],
+            }
+          }
+        }
+      ]
+    }
   };
 };
