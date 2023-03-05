5b70fed4d44e6979a6701b5dc25dac85d21fe6d5
erech <sofiahanapin@gmail.com>
2023-03-05 12:00:43 -0800

    add todos in install.js

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -2,10 +2,30 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault()
+
+    //store for later use + take out hidden class
+    deferredPrompt = event
+    butInstall.classList.toggle('hidden', false)
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    if ('deferredPrompt' in window) {
+        const promptEvent = window.deferredPrompt
+        //Show prompt
+        promptEvent.prompt()
+
+        //prompt is a one time use. This resets deferred prompt
+        window.deferredPrompt = null
+        butInstall.classList.toggle('hidden', true)
+    }
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    console.log("Installed!")
+
+    window.deferredPrompt = null
+});
