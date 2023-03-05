9175a2f5239a20aa30ccd6cbaa41723606a3f18e
erech <sofiahanapin@gmail.com>
2023-03-05 12:53:22 -0800

    reconfigured files

--- Develop/client/client/src-sw.js
+++ Develop/client/client/src-sw.js
@@ -27,4 +27,25 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
+// Cache assets with CacheFirst method
+const assetCache = new CacheFirst({
+  cacheName: 'asset-cache',
+  plugins: [
+    new CacheableResponsePlugin({
+      statuses: [0, 200],
+    }),
+    new ExpirationPlugin({
+      maxAgeSeconds: 5 * 20 * 55 * 55,
+    }),
+  ],
+});
+
+// Register the route for all GET requests for assets
+registerRoute(
+  ({ request }) => request.destination === 'script' ||
+                   request.destination === 'style' ||
+                   request.destination === 'image',
+  assetCache
+);
+
 registerRoute();
